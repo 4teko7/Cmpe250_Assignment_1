f148bebe372bcd1b8eb71b5f731b96aa5953f76e
teko <4teko7@gmail.com>
2020-01-09 02:03:37 +0300

    I implemented Execute Method

--- Restaurant.cpp
+++ Restaurant.cpp
@@ -1,6 +1,6 @@
 /*
-Student Name: 
-Student Number: 
+Student Name: Bilal Tekin
+Student Number: 2017400264
 Project Number: 1
 Compile Status: [SUCCESS/FAIL]
 Running Status: [SUCCESS/FAIL]
@@ -16,6 +16,26 @@ void Restaurant::execute(const vector<string>& operations) {
         cout << "Operation: " << op << "\t"; 
         
 		/* YOU NEED TO IMPLEMENT THIS PART */
+        if(op == "N"){
+            for (int i = 0; i < tables.size(); i++) {
+                if(!tables.at(i).isOccupied()){
+                    tables.at(i).occupy();
+                    break;
+                }
+            }
+        }else if(op == "S"){
+            for (int i = tables.size()-1; i >= 0; i--) {
+                if(!tables.at(i).isOccupied()){
+                    tables.at(i).occupy();
+                    break;
+                }
+            }
+        }else{
+            //stringstream geek(op);
+            //geek >> x;
+            tables.at(std::stoi(op)-1).empty();
+
+        }
 
         cout << "State: " << *this << endl;
     }
